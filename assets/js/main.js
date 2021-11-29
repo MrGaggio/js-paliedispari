8a02ca4fe5534350a9f81e228e36cc7b86f09040
MrGaggio <steruggiu@gmail.com>
2021-11-29 14:53:04 +0000

    prove primo esercizio, passo al secondo e dopo lo riprendo

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+(prima senza funzione e poi con funzione */
+
+
+   
+
+    //Chiedere all’utente di inserire una parola
+    let parola = prompt('Inserisci una parola per verificare se è palindroma')
+
+ 
+    const palindromo = parola.split().reverse().join()
+
+
+    function checkPalindromo (parola) {
+        
+        if (parola == palindromo) {
+            console.log(parola + " è un palindromo");
+        } else (parola + " non è un palindromo") 
+
+    }
+
+    const verifica = checkPalindromo(parola)
+    console.log(verifica);
