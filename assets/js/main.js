d80589ba6a6ee755a59c5e8f530cbc269a9a5480
MrGaggio <steruggiu@gmail.com>
2021-11-29 16:58:40 +0000

    non riesco a farlo funzionare provo + tardi

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,18 +9,14 @@ Creare una funzione per capire se la parola inserita è palindroma
     //Chiedere all’utente di inserire una parola
     let parola = prompt('Inserisci una parola per verificare se è palindroma')
 
+
   
- /*    const palindromo = parola.split().reverse().join()
-
-
-    function checkPalindromo (parola) {
-        
-        if (parola == palindromo) {
-            console.log(parola + " è un palindromo");
-        } else (parola + " non è un palindromo") 
-
-        
+    function reverse(parola){
+        if(parola == parola.reverse()) {
+            console.log("è un palindromo");
+        }
     }
 
 
-    
+    const palindromo = reverse(parola)
+    console.log(palindromo);
